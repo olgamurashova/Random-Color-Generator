76468196d72f2428aed799c9a5b645d14f9f66fe
Olga Murashova <om.dreyfus@gmail.com>
2022-09-13 22:55:09 -0400

    main.js

--- main.js
+++ main.js
@@ -6,3 +6,34 @@ for (let index = 0; index < 20; index++) {
     container.appendChild(colorContainer);
     
 }
+
+function randomColor(){
+    const charSet = "0123456789abcdef";
+    const colorCodeLength = 6;
+    let colorCode = "";
+    for (let index = 0; index < colorCodeLength; index++) {
+        const randomNum = Math.floor(Math.random() * charSet.length);
+        //console.log(randomNum);
+        colorCode +=charSet.substring(randomNum, randomNum + 1);
+        // console.log(colorCode,  randomNum);
+        
+    }
+    return colorCode;
+}
+
+randomColor()
+
+const colorContainerAll = document.querySelectorAll(".color-container");
+//console.log(colorContainerAll);
+
+function generateColors(){
+    colorContainerAll.forEach((colorContainerEl)=>{
+        const newColorCode = randomColor();
+        //console.log(newColorCode);
+        colorContainerEl.style.backgroundColor = "#" + newColorCode;
+        colorContainerEl.innerText = "#" + newColorCode;
+
+    })
+}
+
+generateColors();
